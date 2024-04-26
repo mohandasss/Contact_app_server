7a523eb0b15a8dc1734925c9236433f566da9105
Mohan Das <gamermohan39@gmail.com>
2024-04-26 18:55:20 +0530

    added docker

--- src/main/java/io/getarrays/contactapi/constant/Constant.java
+++ src/main/java/io/getarrays/contactapi/constant/Constant.java
@@ -3,7 +3,5 @@ package io.getarrays.contactapi.constant;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
-
-
     public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
